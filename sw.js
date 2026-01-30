ff4a1f49eda555343fdf5a56f067f6b2b252b0e8
FEDEpe8 <fhernansperoni@gmail.com>
2026-01-30 14:55:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v14'; // <--- Siempre actualizar si se cambia el index a una version +
+const CACHE_NAME = 'muni-chascomus-v15'; // <--- Siempre actualizar si se cambia el index a una version +
 const assets = [
   './',
   './index.html',
