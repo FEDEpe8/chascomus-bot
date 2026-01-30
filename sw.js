21336025e95fcbe396cf6a0266e82cf1ab956838
FEDEpe8 <fhernansperoni@gmail.com>
2026-01-30 11:02:08 -0300

    Change CACHE_NAME to 'muni-chascomus-v10'
    Updated cache name to reflect new version.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v9'; // <--- Siempre actualizar si se cambia el index a una version +
+const CACHE_NAME = 'muni-chascomus-v10'; // <--- Siempre actualizar si se cambia el index a una version +
 const assets = [
   './',
   './index.html',
