9fe1f63c3f244b3d73dc89e809fbeac5a24205b0
FEDEpe8 <fhernansperoni@gmail.com>
2026-01-30 23:41:17 -0300

    Change CACHE_NAME to 'muni-chascomus-v22'
    Updated cache name to version 22.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v21'; // <--- Siempre actualizar si se cambia el index a una version +
+const CACHE_NAME = 'muni-chascomus-v22'; // <--- Siempre actualizar si se cambia el index a una version +
 const assets = [
   './',
   './index.html',
