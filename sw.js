dc444eebbe89e35f7cff07d16aa4aeb877d4e865
FEDEpe8 <fhernansperoni@gmail.com>
2026-02-03 00:05:41 -0300

    Change CACHE_NAME to 'muni-chascomus-v44'
    Updated cache name to version 44.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v43'; // <--- Siempre actualizar si se cambia el index a una version +
+const CACHE_NAME = 'muni-chascomus-v44'; // <--- Siempre actualizar si se cambia el index a una version +
 const assets = [
   './',
   './index.html',
