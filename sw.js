fb396385f59545b84259e56d4bb0471f78bf8fb8
FEDEpe8 <fhernansperoni@gmail.com>
2026-02-02 22:03:49 -0300

    Increment CACHE_NAME to 'muni-chascomus-v37'
    Updated cache version number for service worker.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muni-chascomus-v36'; // <--- Siempre actualizar si se cambia el index a una version +
+const CACHE_NAME = 'muni-chascomus-v37'; // <--- Siempre actualizar si se cambia el index a una version +
 const assets = [
   './',
   './index.html',
